2ff6da29782c2bc54bd7274c8c367f7190c8ab34
João Henrique de Oliveira Júnior <joao.holiveira@totvs.com.br>
2020-02-07 15:51:17 -0300

    Trabalhando com proxy factory

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,23 +9,20 @@ class NegociacaoController {
 
         let self = this;
 
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-            get(target, prop, receiver) {
-                if (['adiciona', 'esvazia'].includes(prop) && typeof (target[prop]) === typeof (Function)) {
-                    return function () {
-                        Reflect.apply(target[prop], target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-
-                return Reflect.get(target, prop, receiver);
-            }
-        });
+        this._listaNegociacoes = ProxyFactory.create(
+            new ListaNegociacoes(),
+            ['adiciona', 'esvazia'], 
+            (model) => this._negociacoesView.update(model)
+        );
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        
+        this._mensagem = ProxyFactory.create(
+            new Mensagem(),
+            ['texto'],
+            (model) => this._mensagemView.update(model)
+        );
+
+        this._mensagemView = new MensagemView($('#mensagemView'));  
         this._mensagemView.update(this._mensagem);
         this._negociacoesView.update(this._listaNegociacoes);
     }
