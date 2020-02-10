2c21a354f128e80ced59f625584821933f50014e
João Henrique de Oliveira Júnior <joao.holiveira@totvs.com.br>
2020-02-10 17:45:11 -0300

    Utilizando o fetch API

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,20 +1,22 @@
 class HttpService {
 
+    _handleErrors(res) {
+        if(!res.ok) throw new Error(res.statusText);    
+        return res;
+    }
+
     get(url) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json())
+    }
 
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState === 4) {
-                    if (xhr.status !== 200)
-                        reject(xhr.responseText);
-
-                    resolve(JSON.parse(xhr.responseText));
-                }
-            };
-
-            xhr.send();
-        });
+    post(url, dado) {
+        return fetch(url, {
+            method: 'post',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res))
     }
 }
