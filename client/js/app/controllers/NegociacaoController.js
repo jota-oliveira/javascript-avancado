e9ee9c010dbba4a1cf121920df2b2c0e3b72b1e9
João Henrique de Oliveira Júnior <joao.holiveira@totvs.com.br>
2020-02-10 08:53:11 -0300

    Aplicando padrão de projeto Promise

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -53,18 +53,19 @@ class NegociacaoController {
     importaNegociacoes() {
         let service = new NegociacaoService();
 
-        service.obterNegociacoesDaSemana((erro, negociacoes) => {
-            if(erro) {
-                this._mensagem.texto = erro;
-                return;
-            }
-
-            negociacoes.forEach(
-                negociacao => this._listaNegociacoes.adiciona(negociacao)
-            );
-
-            this._mensagem.texto = "Negociações importadas com sucesso";
-        });
+        Promise.all([
+            service.obterNegociacoesDaSemana(),
+            service.obterNegociacoesDaSemanaAnterior(),
+            service.obterNegociacoesDaSemanaRetrasada()
+        ]).then(negociacoes => {
+            const todasAsNegociacoes = negociacoes
+                .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+                .forEach(negociacao => {
+                    this._listaNegociacoes.adiciona(negociacao);
+                    this._mensagem.texto = "Negociações adicionadas com sucesso!";
+                });
+        })
+        .catch(error => { this._mensagem.texto = error; });
     }
 
     /* Estados de requisição da biblioteca XMLHttpRequest 
--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -1,19 +1,61 @@
 class NegociacaoService {
-    obterNegociacoesDaSemana(cb) {
-        let xhr = new XMLHttpRequest();
-
-        xhr.open('GET', 'negociacoes/semana');
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) {
-                if (xhr.status !== 200) {
-                    return cb('As negociações não puderam ser importadas', null);
+    obterNegociacoesDaSemana() {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+    
+            xhr.open('GET', 'negociacoes/semana');
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4) {
+                    if (xhr.status !== 200) {
+                        reject('As negociações não puderam ser importadas');
+                    }
+    
+                    resolve(JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
                 }
+            };
+    
+            xhr.send();
+        });
+    }
 
-                return cb(null, JSON.parse(xhr.responseText)
-                    .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-            }
-        };
+    obterNegociacoesDaSemanaAnterior() {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
 
-        xhr.send();
+            xhr.open('GET', 'negociacoes/anterior');
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4) {
+                    if (xhr.status !== 200) {
+                        reject('As negociações não puderam ser importadas');
+                    }
+
+                    resolve (JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                }
+            };
+
+            xhr.send();
+        });
+    }
+
+    obterNegociacoesDaSemanaRetrasada() {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+    
+            xhr.open('GET', 'negociacoes/retrasada');
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4) {
+                    if (xhr.status !== 200)
+                        reject('As negociações não puderam ser importadas');
+    
+                    resolve(JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                }
+            };
+    
+            xhr.send();
+        });
+       
     }
 }
